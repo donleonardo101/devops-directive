34d3085e3edec146c62b16a4d53ca0b1c1f5f1e9
chrisgiem <krzysztofgiemza1@gmail.com>
2021-09-30 17:18:58 +0000

    2

--- 2020-12-27-productionize-mern/server/db/index.js
+++ 2020-12-27-productionize-mern/server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGO_URI;
+const connectionString = 'mongodb://mongo:27017/cinema'
 
 mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
   console.error('Connection error', e.message);
